a20413319a2f9fc1bbf0f1700ec0eeb5131d5039
unknown <xin90210@gmail.com>
2019-10-30 22:17:42 +0200

    sync1

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-var items = [
-    [1, 2],
-    [3, 4],
-    [5, 6]
-]
 
-function arrTable(arr, domPointer) {
-    document.getElementById('domPointer').innerHTML = 
-
-    function tablica(arr) {
-        document.write("<table>");
-    for ( var i = 0; i < arr.length; i++) {
-        document.write("<tr>");
-        for ( var j = 0; j < arr[i].length; j++) {
-            document.write("<td>" + arr[i][j] + "</td>");
-                }
-            document.write("</tr>");
-            }
-        document.write("</table>");
+function arrTable(arr, element) {
+    const table = document.createElement("table")
+    element.appendChild(table)
+    for (let i = 0; i < arr.length; i++) {
+        const tr = document.createElement("tr")
+        table.appendChild(tr)
+        for (let j = 0; j < arr[i].length; j++) {
+            const td = document.createElement("td")
+            td.innerText = arr[i][j]
+            tr.appendChild(td)
         }
-        tablica(arr);
+    }
 }
 
-arrTable(items, sample)
